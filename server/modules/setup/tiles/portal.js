b8d6e397f53c96207587615de59e7d1926c8f138
Glitch (stealth-humane-diamond) <none>
2024-09-20 10:44:59 +0000

    👣🎁 Checkpoint

--- server/modules/setup/tiles/portal.js
+++ server/modules/setup/tiles/portal.js
@@ -1,11 +1,3 @@
-/*jslint node: true */
-/*jshint -W061 */
-/*global goog, Map, let */
-//"use strict";
-// General requires
-require('google-closure-library');
-goog.require('goog.structs.PriorityQueue');
-goog.require('goog.structs.QuadTree');
 
 let launchForce = 1500,
     gravity = 13500,
