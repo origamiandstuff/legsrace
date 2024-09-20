69172212e31009d10a410cd07d7c33f73f51e6fe
Glitch (stealth-humane-diamond) <none>
2024-09-20 07:41:48 +0000

    👵📟 Checkpoint ./server/config.js:69809645/32

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "stealth-humane-diamond.glitch.me",
+    host: "",
 
     // Which port to run the web server on.
     port: 8080,
