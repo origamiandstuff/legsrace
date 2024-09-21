9a6d1a5e732619b1ab90821a8a9b95765979a5ba
Glitch (stealth-humane-diamond) <none>
2024-09-21 06:41:01 +0000

    💪🍛 Checkpoint ./server/modules/definitions/groups/tanks.js:69809645/56 ./server/modules/definitions/addons/armsRace.js:69809645/445

--- server/modules/definitions/addons/armsRace.js
+++ server/modules/definitions/addons/armsRace.js
@@ -1,15 +1,18 @@
 const { combineStats, makeAuto, makeOver, makeDeco, makeGuard, makeBird, makeRadialAuto, weaponArray } = require('../facilitators.js');
 const { base, statnames, dfltskl, smshskl } = require('../constants.js');
-const g = require('../gunvals.js');
-require('../generics.js');
+const { g } = require('../gunvals.js')
 
-return console.log('[Arms Race Addon] Disabled by default.');
+//return console.log('[Arms Race Addon] Disabled by default.');
 
 // Removes desmos
-Class.basic.UPGRADES_TIER_0 = Class.basic.UPGRADES_TIER_0.filter(basic => basic !== 'desmos');
+Class.basic.UPGRADES_TIER_1 = Class.basic.UPGRADES_TIER_1.filter(basic => basic !== 'desmos');
+Class.basic.UPGRADES_TIER_1.push('single');
+Class.assassin.UPGRADES_TIER_3 = Class.assassin_UPGRADES_TIER_3.filter(assassin => assassin !== 'single');
 
 // Branches
 
 
 
 // Tanks
+
+console.log('[Arms Race Addon] Loaded Arms Race.')
