073af8edf442cf90b13acbf003b8ffc4b4212d17
Glitch (stealth-humane-diamond) <none>
2024-09-21 07:25:45 +0000

    🎡🍤 Checkpoint ./server/modules/definitions/addons/armsRace.js:66976691/253:69809645/1474

--- server/modules/definitions/addons/armsRace.js
+++ server/modules/definitions/addons/armsRace.js
@@ -12,7 +12,7 @@ Class.basic.UPGRADES_TIER_2.push('single', 'smasher');
 
 // Branches
 
-Class.single.UPGRADES_TIER_3 = ['duo','autoSingle'];
+Class.single.UPGRADES_TIER_3 = ['duo', 'sharpshooter', 'autoSingle'];
 
 // Tanks
 
@@ -48,10 +48,50 @@ Class.duo = {
   
 };
 
-Class.autoSingle = makeAuto('single');
+Class.sharpshooter = {
+    PARENT: "genericTank",
+    LABEL: "Sharpshooter",
+    DANGER: 7,
+    GUNS: [
+        {
+            POSITION: [24, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.single, g.sniper]),
+                TYPE: "bullet"
+            }
+        },
+        {
+            POSITION: [5.5, 8, -1.8, 6.5, 0, 0, 0]
+        }
+    ]
+}
+Class.ternion = {
+    PARENT: "genericTank",
+    LABEL: "Ternion",
+    BODY: {
+        SPEED: 1.1 * base.SPEED
+    },
+    GUNS: weaponArray({
+        POSITION: {
+            LENGTH: 18,
+            WIDTH: 8
+        },
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.single]),
+            TYPE: "bullet"
+        },
+        POSITION: [5.5, 8, -1.8, 6.5, 0, 0, 0]
+    }, 3)
+}
 
-// Sidewinders
-Class.hognoseMissile = makeAuto("snakeOld", "Hognose missile", {size: 7.5, type: 'droneAutoTurret'});
+Class.avian = makeBird('single', 'Avian');
+Class.custodian = makeGuard('single', 'Custodian')
+Class.assistant = makeOver('single', 'Assistant', {count: 1, independent: true, cycle: false})
+Class.autoSingle = makeAuto('single');
+//Sidewinders
+
+
+Class.hognoseMissile = makeAuto("snakeOld", "Hognose missile", {type: "pillboxTurret"});
 Class.hognose = {
     PARENT: "genericTank",
     LABEL: "Hognose",
