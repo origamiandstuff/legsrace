3014a00dbabe7619c8151f90740e47524b8cbe15
Glitch (simple-arras-template-server) <none>
2024-09-22 05:32:31 +0000

    😎🚨 Checkpoint ./server/modules/definitions/addons/armsRace.js:69809645/2199

--- server/modules/definitions/addons/armsRace.js
+++ server/modules/definitions/addons/armsRace.js
@@ -10,58 +10,13 @@ const g = require('../gunvals.js')
 Class.assassin.UPGRADES_TIER_3 = Class.assassin.UPGRADES_TIER_3.filter(assassin => assassin !== 'single');
 Class.basic.UPGRADES_TIER_1 = Class.basic.UPGRADES_TIER_1.filter(basic => basic !== 'desmos');
 Class.basic.UPGRADES_TIER_2.push('single');
-Class.addons.UPGRADES_TIER_0.push('armsRace');
 
-// Menu
-Class.armsRace = {
-    PARENT: "genericTank",
-    LABEL: "Arms Race Menu",
-    BODY: {
-        SHIELD: 1000,
-        REGEN: 10,
-        HEALTH: 100,
-        DAMAGE: 10,
-        DENSITY: 20,
-        FOV: 2,
-    },
-    SKILL_CAP: Array(10).fill(dfltskl),
-    IGNORED_BY_AI: true,
-    RESET_CHILDREN: true,
-    ACCEPTS_SCORE: true,
-    CAN_BE_ON_LEADERBOARD: true,
-    CAN_GO_OUTSIDE_ROOM: false,
-    IS_IMMUNE_TO_TILES: false,
-    DRAW_HEALTH: true,
-    ARENA_CLOSER: true,
-    INVISIBLE: [0, 0],
-    ALPHA: [0, 1],
-    HITS_OWN_TYPE: "hardOnlyTanks",
-    NECRO: false,
-    SHAPE: [
-        [-1, -0.8],
-        [-0.8, -1],
-        [0.8, -1],
-        [1, -0.8],
-        [0.2, 0],
-        [1, 0.8],
-        [0.8, 1],
-        [-0.8, 1],
-        [-1, 0.8],
-    ],
-    GUNS: [
-        {
-            POSITION: [18, 10, -1.4, 0, 0, 0, 0],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.op]),
-                TYPE: "developerBullet"
-            }
-        }
-    ],
-}
+
 // Singles
 Class.duo = {
   PARENT: 'genericTank',
   LABEL: 'Duo',
+  DANGER: 7,
   GUNS: [
         {
             POSITION: {
@@ -110,6 +65,7 @@ Class.sharpshooter = {
 Class.ternion = {
     PARENT: "genericTank",
     LABEL: "Ternion",
+    DANGER: 7,
     BODY: {
         SPEED: 1.1 * base.SPEED
     },
@@ -228,7 +184,52 @@ Class.hognose = {
 // Assassins
 Class.buttbuttin = addBackGunner('assassin', 'Buttbuttin');
 Class.hitman = makeOver('assassin', 'Hitman', {count: 1, independent: !0, cycle: !1});
-Class.sniper3 = makeRadialAuto('sniperAutoTankGun', { isTurret: !0, body: { FOV: 1.2 }, danger: 7, label: 'Sniper-3' ,count:3 }); 
+Class.sniper3 = makeRadialAuto('sniperAutoTankGun', { isTurret: !0, body: { FOV: 1.2 }, danger: 7, label: 'Sniper-3' ,count:3 });
+
+// Rangers
+Class.autoRanger = makeAuto('ranger');
+Class.vindicator = {
+    PARENT: "genericTank",
+    LABEL: "Vindicator",
+    DANGER: 7,
+    BODY: {
+        SPEED: 0.8 * base.SPEED,
+        FOV: 1.8 * base.FOV,
+    },
+    GUNS: [
+        {
+            POSITION: [35, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.sniper, g.assassin]),
+                TYPE: "bullet",
+            },
+        },
+        {
+            POSITION: [5, 8, -1.4, 8, 0, 0, 0],
+        },
+    ],
+}
+Class.hawker = {
+    PARENT: "genericTank",
+    DANGER: 7,
+    LABEL: "Hawker",
+    BODY: {
+        SPEED: 0.85 * base.SPEED,
+        FOV: 1.35 * base.FOV
+    },
+    INVISIBLE: [0.08, 0.03],
+    TOOLTIP: "Stay still to turn invisible.",
+    GUNS: [
+        {
+            POSITION: [32, 8, -1.8, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.sniper, g.assassin]),
+                TYPE: "bullet"
+            }
+        }
+    ]
+}
+
 // Twins
 
 
