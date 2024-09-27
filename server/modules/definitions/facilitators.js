1f12a57cbc83f48fe8e4bad3abba66ed4c46c77b
Glitch (simple-arras-template-server) <none>
2024-09-27 12:39:11 +0000

    🐮👶 Checkpoint ./server/modules/definitions/facilitators.js:69809645/4

--- server/modules/definitions/facilitators.js
+++ server/modules/definitions/facilitators.js
@@ -790,7 +790,7 @@ exports.makeCrasher = type => ({
     HAS_NO_MASTER: true,
     VALUE: type.VALUE * 5,
     BODY: {
-        SPEED: 1 + 5 / Math.max(2, (type.PROPS.length ?? 0) + type.SHAPE),
+        SPEED: 1 + 5 / Math.max(2, (type.PROPS.length ?? 0) + type.SHAPE / 2),
         HEALTH: Math.pow(type.BODY.HEALTH, 2/3),
         DAMAGE: Math.pow(type.BODY.HEALTH, 1/3) * type.BODY.DAMAGE,
         ACCELERATION: 5,
